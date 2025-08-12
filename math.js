2d13f84dc86aaecf2ed89e2dd776757adde1a378
Kazi Fabiha Golam Liya <Kazifabihaliya@gmail.com>
2025-08-12 23:12:20 +0600

    progress

---
+++ math.js
@@ -0,0 +1,15 @@
+const min = Math.min(45,55,2,1,87,696,484,63,-2)
+console.log(min)
+const max = Math.max(45,55,2,1,87,696,484,63,-2)
+console.log(max)
+
+console.log(Math.PI)
+console.log(Math.abs(-8))
+console.log(Math.round(8.48)) //purnoshongkhay rupantor
+console.log(Math.ceil(8.48)) 
+console.log(Math.floor(8.48)) 
+console.log(Math.random()) 
+const rand = Math.round(Math.random()*10) 
+console.log(rand)
+//ceil - upper limit 
+//floor - lower limit
